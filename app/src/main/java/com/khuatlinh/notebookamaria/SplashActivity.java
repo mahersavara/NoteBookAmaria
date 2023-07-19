9c037b1cbf1ef4c041935ed8a548cbf4f412bcb2
mahersevara <kdlinh8811@gmail.com>
2023-07-19 20:20:42 +0700

    Create & Functioning the SplashActivity by postDelay Runnable (Completed)

--- app/src/main/java/com/khuatlinh/notebookamaria/SplashActivity.java
+++ app/src/main/java/com/khuatlinh/notebookamaria/SplashActivity.java
@@ -2,7 +2,9 @@ package com.khuatlinh.notebookamaria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                // Intent bắn dữ liệu -> class đằng sau (activity )
+            }
+
+        }, 1000);
+
     }
 }
